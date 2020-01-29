b8ee0c363cd7e9cf6e35fe5372b4c30a65ba49cd
Alvaro Armas <aarmas92@outlook.com>
2020-01-29 02:03:29 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,8 @@
 #write your code here
-def countdown_output
+def using_while
 	countdown = 12	
 	while countdown < 12	
-		puts "#{number} SECOND(S)!"	
+		puts "#{countdown} SECOND(S)!"	
 		countdown -= 1	
 	end	
 
