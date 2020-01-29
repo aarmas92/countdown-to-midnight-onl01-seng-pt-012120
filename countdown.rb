80f1dd7f8d25b098978678c2384d6ddbbf740729
Alvaro Armas <aarmas92@outlook.com>
2020-01-29 01:59:37 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,11 @@
 #write your code here
-number = 0
+def countdown	
+	countdown = 12	
+	while countdown < 12	
+		puts "#{countdown} SECOND(S)!"	
+		countdown -= 1	
+	end	
+end
 
-while number < 10
-
-
-def countdown
+puts "HAPPY NEW YEAR!"
 end
